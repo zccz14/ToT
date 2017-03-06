dcf847131a54d8f5a75a93cd6e4e883656a92abe
zccz14 <zccz14@outlook.com>
2017-03-06 16:48:03 +0800

    Import Material-UI

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import injectTapEventPlugin from "react-tap-event-plugin";
+
+injectTapEventPlugin();
+
+const Root = () => (
+    <MuiThemeProvider>
+        <App/>
+    </MuiThemeProvider>
+);
 
 ReactDOM.render(
-    <App />,
+    <Root />,
     document.getElementById('root')
 );
