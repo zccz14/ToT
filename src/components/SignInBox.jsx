c085b41b890641eaf9115a121a6ae6cd6764bca0
zccz14 <zccz14@outlook.com>
2017-03-10 13:56:19 +0800

    press Enter to submit form in SignInBox

--- src/components/SignInBox.jsx
+++ src/components/SignInBox.jsx
@@ -3,27 +3,37 @@ import TextField from "material-ui/TextField";
 import RaisedButton from "material-ui/RaisedButton";
 
 class SignInBox extends Component {
+  submit = () => {
+    this.props.onSignIn(this.refs.username.input.value, this.refs.password.input.value)
+  };
+
+  onKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      this.submit();
+    }
+  };
+
   render() {
     return (
       <div className="center">
         <TextField
           floatingLabelText="Your Username"
           ref="username"
+          onKeyDown={this.onKeyDown}
         />
         <br/>
         <TextField
           floatingLabelText="Your Password"
           type="password"
           ref="password"
+          onKeyDown={this.onKeyDown}
         />
         <br/>
         <RaisedButton
           label="Sign In"
           primary={true}
           className="sign-in-button"
-          onClick={() => {
-            this.props.onSignIn(this.refs.username.input.value, this.refs.password.input.value)
-          }}
+          onClick={this.submit}
         />
       </div>
     );
