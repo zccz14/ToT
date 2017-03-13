548fff73b44e2614cde563ce998c7cc7bafa460a
zccz14 <zccz14@outlook.com>
2017-03-14 00:58:50 +0800

    vivid problem type display

--- src/components/ProblemBuilder.jsx
+++ src/components/ProblemBuilder.jsx
@@ -4,24 +4,59 @@ import TextField from "material-ui/TextField";
 import FlatButton from "material-ui/FlatButton";
 import SelectField from "material-ui/SelectField";
 import MenuItem from "material-ui/MenuItem";
+import Checkbox from "material-ui/Checkbox";
+import {RadioButton, RadioButtonGroup} from "material-ui/RadioButton";
+import Subheader from "material-ui/Subheader";
+import Chip from "material-ui/Chip";
 
 class ProblemBuilder extends Component {
   state = {
-    type: "OPEN_QUESTION"
+    type: "OPEN_QUESTION",
+    choice: []
   };
   handleChange = (event, index, value) => this.setState({type: value});
+
+  getAnswer = () => {
+    switch (this.state.type) {
+      case 'OPEN_QUESTION':
+        return this.refs.answer.input.refs.input.value;
+      case 'TRUE_OR_FALSE':
+        return this.refs.answerTrueOrFalse.state.switched;
+      default:
+        return null;
+    }
+  };
+
   onCreate = () => {
     console.log(this.refs.description.input);
     this.props.onCreate({
       title: this.refs.title.input.value,
       description: this.refs.description.input.refs.input.value, // for multiLine TextField
       type: this.state.type,
-      referenceAnswer: this.refs.answer.input.refs.input.value
+      referenceAnswer: this.getAnswer()
     });
   };
 
   onCancel = () => {
     this.props.onCancel();
+  };
+
+  addChoice = () => {
+    this.setState({
+      choice: [
+        ...this.state.choice,
+        this.refs.add_choice.input.value
+      ]
+    });
+  };
+
+  removeChoice = (i) => {
+    this.setState({
+      choice: [
+        ...this.state.choice.slice(0, i),
+        ...this.state.choice.slice(i + 1)
+      ]
+    });
   };
 
   render() {
@@ -61,14 +96,110 @@ class ProblemBuilder extends Component {
           fullWidth={true}
           rows={3}
         />
-        <TextField
-          floatingLabelText="Reference Answer"
-          hintText="Type answer here."
-          ref="answer"
-          multiLine={true}
-          fullWidth={true}
-          rows={3}
-        />
+        {
+          this.state.type === 'TRUE_OR_FALSE' ?
+            <Checkbox
+              ref="answerTrueOrFalse"
+              label="Reference Answer"
+              labelPosition="left"
+            />
+            : null
+        }
+        {
+          this.state.type === 'OPEN_QUESTION' ?
+            <TextField
+              floatingLabelText="Reference Answer"
+              hintText="Type answer here."
+              ref="answer"
+              multiLine={true}
+              fullWidth={true}
+              rows={3}
+            />
+            : null
+        }
+        {
+          this.state.type === 'MULTIPLE_CHOICE_SINGLE_ANSWER' ?
+            <div>
+              <Subheader>Choice</Subheader>
+              <div style={{
+                display: 'flex',
+                flexWrap: 'wrap'
+              }}>
+                {
+                  this.state.choice.map((v, i) => (
+                    <Chip
+                      key={i}
+                      onRequestDelete={() => {
+                        this.removeChoice(i);
+                      }}
+                      style={{margin: 5}}
+                    >
+                      {v}
+                    </Chip>
+                  ))
+                }
+              </div>
+              <Subheader>Reference Answer</Subheader>
+              <RadioButtonGroup
+                name="answerSingleAnswer"
+              >
+                {
+                  this.state.choice.map((v, i) => (
+                    <RadioButton key={i} value={i} label={v}/>
+                  ))
+                }
+              </RadioButtonGroup>
+              <br/>
+              <TextField
+                ref="add_choice"
+                floatingLabelText="Add Choice"
+                onKeyDown={(e) => e.key === 'Enter' && this.addChoice()}
+              />
+            </div>
+            : null
+        }
+        {
+          this.state.type === 'MULTIPLE_CHOICE_MULTIPLE_ANSWERS' ?
+            (
+              <div>
+                <Subheader>Choice</Subheader>
+                <div style={{
+                  display: 'flex',
+                  flexWrap: 'wrap'
+                }}>
+                  {
+                    this.state.choice.map((v, i) => (
+                      <Chip
+                        key={i}
+                        onRequestDelete={() => {
+                          this.removeChoice(i);
+                        }}
+                        style={{margin: 5}}
+                      >
+                        {v}
+                      </Chip>
+                    ))
+                  }
+                </div>
+                <Subheader>Reference Answer</Subheader>
+                {
+                  this.state.choice.map((v, i) => (
+                    <Checkbox
+                      key={i}
+                      label={v}
+                    />
+                  ))
+                }
+                <br/>
+                <TextField
+                  ref="add_choice"
+                  floatingLabelText="Add Choice"
+                  onKeyDown={(e) => e.key === 'Enter' && this.addChoice()}
+                />
+              </div>
+            )
+            : null
+        }
         <br/>
         <FlatButton
           label="create"
