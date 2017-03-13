9d8550c3bec2ee0c1af6c45cd06b4342191416c7
zccz14 <zccz14@outlook.com>
2017-03-13 13:53:41 +0800

    Problem Create

--- src/components/ProblemBuilder.jsx
+++ src/components/ProblemBuilder.jsx
@@ -2,13 +2,20 @@ import React, {Component} from "react";
 import Paper from "material-ui/Paper";
 import TextField from "material-ui/TextField";
 import FlatButton from "material-ui/FlatButton";
+import SelectField from "material-ui/SelectField";
+import MenuItem from "material-ui/MenuItem";
 
-class ProblemEditor extends Component {
+class ProblemBuilder extends Component {
+  state = {
+    type: "OPEN_QUESTION"
+  };
+  handleChange = (event, index, value) => this.setState({type: value});
   onCreate = () => {
     console.log(this.refs.description.input);
     this.props.onCreate({
       title: this.refs.title.input.value,
-      description: this.refs.description.input.refs.input.value // for multiLine TextField
+      description: this.refs.description.input.refs.input.value, // for multiLine TextField
+      type: this.state.type
     });
   };
 
@@ -33,13 +40,25 @@ class ProblemEditor extends Component {
           ref="title"
         />
         <br/>
+        <SelectField
+          value={this.state.type}
+          onChange={this.handleChange}
+          floatingLabelText="Type"
+          fullWidth={true}
+        >
+          <MenuItem value="MULTIPLE_CHOICE_SINGLE_ANSWER" primaryText="Multiple Choice with Single Answer"/>
+          <MenuItem value="MULTIPLE_CHOICE_MULTIPLE_ANSWERS" primaryText="Multiple Choice with Multiple Answers"/>
+          <MenuItem value="TRUE_OR_FALSE" primaryText="True or False"/>
+          <MenuItem value="OPEN_QUESTION" primaryText="Open Question"/>
+        </SelectField>
+        <br/>
         <TextField
           floatingLabelText="Description"
           hintText="Type description here."
           ref="description"
           multiLine={true}
           fullWidth={true}
-          rows={10}
+          rows={3}
         />
         <input type="file" style={{display: 'none'}}/>
         <br/>
@@ -60,4 +79,4 @@ class ProblemEditor extends Component {
   }
 }
 
-export default ProblemEditor;
+export default ProblemBuilder;
--- src/containers/NewProblem.jsx
+++ src/containers/NewProblem.jsx
@@ -1,10 +1,21 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
-import ProblemEditor from "../components/ProblemEditor";
+import ProblemBuilder from "../components/ProblemBuilder";
+import co from "co";
+import URLs from "../url.json";
 
 class NewProblem extends Component {
   onCreate = (args) => {
-    console.log(args);
+    co(function*() {
+      const res = yield fetch(URLs.baseURL + '/problems', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        credentials: 'include',
+        body: JSON.stringify(args)
+      });
+    })
   };
   onCancel = () => {
     this.props.router.push('/dashboard');
@@ -13,7 +24,7 @@ class NewProblem extends Component {
   render() {
     return (
       <div>
-        <ProblemEditor
+        <ProblemBuilder
           onCreate={this.onCreate}
           onCancel={this.onCancel}
         />
